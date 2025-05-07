ee721758d8a231b8f8f0dfe069e27453fbef7140
Iker02 <144890666+Iker02@users.noreply.github.com>
2025-05-08 00:36:42 +0200

    editado el navbar y cambios en el home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
+import { NgxMarqueeComponent } from '@omnedia/ngx-marquee';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ErrorComponent
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, ErrorComponent],
+  imports: [FormsModule, NgxMarqueeComponent, BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration(withEventReplay())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
