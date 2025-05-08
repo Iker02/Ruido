954135b63f58ec7fc6ce36a61b228e28c8d2016e
IkerMadariaga <iker.moreno002@gmail.com>
2025-05-08 15:45:07 +0200

    Add contact form with email sending functionality using EmailJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   BrowserModule,
   provideClientHydration,
@@ -23,6 +23,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     BrowserModule,
     RouterModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     NgxSpinnerModule,
     BrowserAnimationsModule,
   ],
