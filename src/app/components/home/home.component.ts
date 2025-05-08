5ef036dc967a94773a214ebf869d1e74660f0f7f
IkerMadariaga <iker.moreno002@gmail.com>
2025-05-08 12:51:00 +0200

    Add GSAP for marquee animation and remove unused CSS styles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('marqueeContent') marqueeContent!: ElementRef;
 
+  private currentOffset: number = 0;
+  private speed: number = 0.5; // Controla la velocidad del desplazamiento
+
+  ngAfterViewInit(): void {
+    this.startMarquee();
+  }
+
+  startMarquee() {
+    const content = this.marqueeContent.nativeElement;
+    const containerWidth = content.offsetWidth;
+
+    const moveMarquee = () => {
+      // Desplazamos el contenido
+      this.currentOffset -= this.speed;
+
+      // Si el contenido se ha desplazado completamente hacia la izquierda, lo reiniciamos
+      if (this.currentOffset <= -containerWidth) {
+        this.currentOffset = 0;
+      }
+
+      // Aplicamos el desplazamiento
+      content.style.transform = `translateX(${this.currentOffset}px)`;
+
+      // Llamamos de nuevo a moveMarquee para el siguiente cuadro
+      requestAnimationFrame(moveMarquee);
+    };
+
+    // Iniciamos la animación
+    requestAnimationFrame(moveMarquee);
+  }
 }
