e63d95238cff06fcff700ea84dc88418e714a9b9
IkerMadariaga <iker.moreno002@gmail.com>
2025-05-13 16:28:13 +0200

    refactor: update animations and improve layout styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, HostListener, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ButtonModule } from 'primeng/button';
 
@@ -18,52 +19,54 @@ export class AppComponent implements AfterViewInit {
   mensaje = 'Hola, me gustaría recibir información :)';
   numeroWhatsapp = '34644356186'; // Reemplaza con el número de fede
 
-  constructor(private spinner: NgxSpinnerService) {}
-
+  constructor(
+    private spinner: NgxSpinnerService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
   ngOnInit(): void {
-    // Mostrar el spinner tan pronto como la aplicación se inicia
     this.spinner.show();
   }
 
   ngAfterViewInit(): void {
-    // Hacer que el spinner se oculte después de un pequeño retraso (ajustar según sea necesario)
-    setTimeout(() => {
-      this.spinner.hide();
-    }, 2000); // Ocultar después de 1 segundo, por ejemplo
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('load', () => {
+        this.spinner.hide();
+      });
+    }
   }
-
+  
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
-
+  
   closeMenu() {
     this.isMenuOpen = false;
   }
-
+  
   @HostListener('document:click', ['$event'])
   handleClickOutside(event: MouseEvent) {
     const target = event.target as HTMLElement;
-
+    
     if (target.closest('#menu-toggle-btn')) {
       return;
     }
-
+    
     // Si el menú está abierto
     if (this.isMenuOpen) {
       const clickedInsideMenu = target.closest('#sidebar-menu');
-
+      
       // Si NO se hizo clic dentro del menú
       if (!clickedInsideMenu) {
         this.isMenuOpen = false;
       }
     }
   }
-
+  
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 0;
   }
-
+  
   enviarMensaje() {
     const texto = encodeURIComponent(this.mensaje);
     const url = `https://wa.me/${this.numeroWhatsapp}?text=${texto}`;
