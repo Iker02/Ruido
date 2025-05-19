908b8e6653cb90f19cc5b2dd11c83f631c7cee19
IkerMadariaga <iker.moreno002@gmail.com>
2025-05-19 15:39:02 +0200

    feat: add Netlify integration and update app component with marquee and year display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ButtonModule } from 'primeng/button';
 
@@ -33,47 +39,50 @@ export class AppComponent implements AfterViewInit {
       window.addEventListener('load', () => {
         setTimeout(() => {
           this.spinner.hide();
-        }, 500); 
+        }, 500);
       });
     }
   }
-  
-  
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
-  
+
   closeMenu() {
     this.isMenuOpen = false;
   }
-  
+
   @HostListener('document:click', ['$event'])
   handleClickOutside(event: MouseEvent) {
     const target = event.target as HTMLElement;
-    
+
     if (target.closest('#menu-toggle-btn')) {
       return;
     }
-    
+
     // Si el menú está abierto
     if (this.isMenuOpen) {
       const clickedInsideMenu = target.closest('#sidebar-menu');
-      
+
       // Si NO se hizo clic dentro del menú
       if (!clickedInsideMenu) {
         this.isMenuOpen = false;
       }
     }
   }
-  
+
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 0;
   }
-  
+
   enviarMensaje() {
     const texto = encodeURIComponent(this.mensaje);
     const url = `https://wa.me/${this.numeroWhatsapp}?text=${texto}`;
     window.open(url, '_blank');
+  }
+
+  getYear(): number {
+    return new Date().getFullYear();
   }
 }
