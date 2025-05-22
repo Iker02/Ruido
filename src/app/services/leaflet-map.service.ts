3bc97e8a1e8e924199e76e3825dd07fb7f7ba0f7
IkerMadariaga <iker.moreno002@gmail.com>
2025-05-22 16:17:51 +0200

    refactor: updated location to from London to Madrid in LeafletMapService

--- src/app/services/leaflet-map.service.ts
+++ src/app/services/leaflet-map.service.ts
@@ -4,18 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LeafletMapService {
-async initMap(container: HTMLElement): Promise<void> {
-  const leafletModule = await import('leaflet');
-  const L = (leafletModule as any).default || leafletModule;
+  async initMap(container: HTMLElement): Promise<void> {
+    const leafletModule = await import('leaflet');
+    const L = (leafletModule as any).default || leafletModule;
 
-  const map = new L.Map(container, {
-    center: [51.505, -0.09],
-    zoom: 13,
-  });
+    const map = new L.Map(container, {
+      center: [40.4168, -3.7038], 
+      zoom: 13,
+    });
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors',
-  }).addTo(map);
-}
-
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(map);
+  }
 }
